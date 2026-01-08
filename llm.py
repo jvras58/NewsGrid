bab39d170e010d6fa385008ce505e2dd38678a85
jvras58 <jvras@cin.ufpe.br>
2026-01-08 16:38:29 -0300

    fix: corrigir a importação do modelo Groq para manter a consistência no arquivo llm.py

--- llm.py
+++ llm.py
@@ -1,6 +1,6 @@
 from agno.agent import Agent
-from agno.models.groq import Groq
 from settings import settings
+from agno.models.groq import Groq
 
 
 def create_agent(
@@ -9,6 +9,7 @@ def create_agent(
     """
     Função centralizada para criar agentes LLM com configurações padronizadas.
     """
+
     model = Groq(id=model_id, api_key=settings.groq_api_key)
     return Agent(
         model=model,

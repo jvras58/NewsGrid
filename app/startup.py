f4b3f869960a2d68b5ec13e66a7f4c4cc2adfcc1
jvras <jonasilv@emeal.nttdata.com>
2026-01-16 09:08:02 -0300

    fix: corrige a descrição da API para gerenciamento de Workers inteligentes

--- app/startup.py
+++ app/startup.py
@@ -9,16 +9,15 @@ from app.api.analyze.routes import router as analyze_router
 from app.api.auth.routes import router as auth_router
 from app.api.status.routes import router as status_router
 from app.api.user.routes import router as users_router
-from utils.logging import get_logger, setup_logging
+from utils.logging import setup_logging
 from utils.settings import settings
 
 setup_logging()
-logger = get_logger("startup")
 
 
 app = FastAPI(
     title=settings.api_title,
-    description="API para gerenciamento de agentes inteligentes.",
+    description="API para gerenciamento de Workers inteligentes.",
     version=settings.api_version,
     debug=settings.debug,
     docs_url=settings.docs_url,

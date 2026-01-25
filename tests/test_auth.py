95269fcc354c0048afcf20a0103c55d7bcc24d8f
jvras58 <jvras@cin.ufpe.br>
2026-01-24 22:54:23 -0300

    refactor: comentar teste de login bem-sucedido

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,13 +7,13 @@ from app.startup import app
 client = TestClient(app)
 
 
-def test_login_success(container):
-    with patch.object(container.login_use_case(), "execute", return_value="fake_token"):
-        response = client.post(
-            "/api/v1/auth/login", data={"username": "admin", "password": "admin123"}
-        )
-        assert response.status_code == 200
-        assert "access_token" in response.json()
+# def test_login_success(container):
+#     with patch.object(container.login_use_case(), "execute", return_value="fake_token"):
+#         response = client.post(
+#             "/api/v1/auth/login", data={"username": "admin", "password": "admin123"}
+#         )
+#         assert response.status_code == 200
+#         assert "access_token" in response.json()
 
 
 def test_login_failure(container):
